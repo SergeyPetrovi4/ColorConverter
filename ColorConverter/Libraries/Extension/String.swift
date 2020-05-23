725323282e3a10ef4a11f37f7a89a4a418cb6a70
Sergey Krasiuk <developer.krasiuk@gmail.com>
2020-05-23 12:52:12 +0300

    Implemented color history, refactored ConverterColoManager, added extension for HEX validation

---
+++ ColorConverter/Libraries/Extension/String.swift
@@ -0,0 +1,18 @@
+//
+//  String.swift
+//  ColorConverter
+//
+//  Created by Sergey Krasiuk on 23/05/2020.
+//  Copyright © 2020 Sergey Krasiuk. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    var isHexColor: Bool {
+        
+        let predicate = NSPredicate(format: "SELF MATCHES %@", "^([A-Fa-f0-9]{6}|[#A-Fa-f0-9]{7})$")        
+        return predicate.evaluate(with: self)
+    }
+}
